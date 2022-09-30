0919354c8c2e66c350db5a17a1b91dfb3610bffe
Aloux01757 <arthurspring2@gmail.com>
2022-09-30 19:56:24 -0300

    Added tag's

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -7,7 +7,11 @@ public class Score : MonoBehaviour
     int hits = 0;
     private void OnCollisionEnter(Collision other)
     {
-        hits++;
-        Debug.Log("You've collided!" + hits);
+          if(other.gameObject.tag != "Hit")
+          {
+            hits++;
+            Debug.Log("You've collided!" + hits);
+          }
+       
     }
 }
